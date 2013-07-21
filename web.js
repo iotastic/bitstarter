924a725d16bbb67e7c36bb4487a8910e7f3d1f27
iotastic <iotastic@gmail.com>
2013-07-21 23:34:03 +0000

    Edited web.jsX

--- web.js
+++ web.js
@@ -2,9 +2,18 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var test = new Buffer(fs.readFileSync("index.html"),"utf-8");
+//  var test2 = "This is a test";
+//  response.send(test2);
+  response.send(test.toString("utf-8");
 });
+
+/*
+var test = new Buffer(fs.readFileSync("index.html"), "utf-8");
+test.toString("utf-8");
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
